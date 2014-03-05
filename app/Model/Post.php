f96e60067c796664eb97517d4bc19e642d9347bb
roshanbungsee <roshanbungsee@gmail.com>
2014-03-05 14:25:12 +0000

    first commit

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -2,6 +2,13 @@
 
 class Post extends AppModel 
 {
-	
+	public $validate = array(
+        'title' => array(
+            'rule' => 'notEmpty'
+        ),
+        'body' => array(
+            'rule' => 'notEmpty'
+        )
+    );
 }
 ?>
